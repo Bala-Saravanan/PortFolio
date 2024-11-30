5cde5c771c1c442c4c6f134812d04e70dfddd145
Bala-Saravanan <jbalasaravanan174@gmail.com>
2024-11-30 15:01:32 +0530

    FEAT: email

--- Contact/app.js
+++ Contact/app.js
@@ -7,3 +7,31 @@ function hide() {
   const hidebar = document.querySelector(".side-bar");
   hidebar.style.display = "none";
 }
+
+// Replace with your EmailJS User ID
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get form data
+  const formData = new FormData(form);
+  const name = formData.get("first-name");
+  const email = formData.get("email");
+  const message = formData.get("message");
+
+  // Send email via EmailJS
+  emailjs
+    .send("service_57z5vfs", "template_dyvdzkj", {
+      from_name: name,
+      from_email: email,
+      message: message,
+    })
+    .then(() => {
+      alert("Email sent successfully!");
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+      alert("Failed to send email. Try again later.");
+    });
+});
