79d1b3edd8126fd89c72a19eb1ccae31843c99c1
Bala-Saravanan <jbalasaravanan174@gmail.com>
2025-01-18 10:37:38 +0530

    FIX: text alignment

--- Contact/app.js
+++ Contact/app.js
@@ -8,19 +8,15 @@ function hide() {
   hidebar.style.display = "none";
 }
 
-// Replace with your EmailJS User ID
-
 const form = document.getElementById("form");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // Get form data
   const formData = new FormData(form);
   const name = formData.get("first-name");
   const email = formData.get("email");
   const message = formData.get("message");
 
-  // Send email via EmailJS
   emailjs
     .send("service_57z5vfs", "template_dyvdzkj", {
       from_name: name,
